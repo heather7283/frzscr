4f074f8918bff93b26f762282e34856dd1e63641
heather7283 <heather7283@protonmail.com>
2024-11-30 00:48:00 +0400

    feat: implement -c option

--- frzscr.c
+++ frzscr.c
@@ -3,6 +3,7 @@
 #include <getopt.h>
 #include <errno.h>
 #include <unistd.h>
+#include <wait.h>
 #include <sys/epoll.h>
 #include <sys/signalfd.h>
 #include <wayland-util.h>
@@ -18,8 +19,11 @@ unsigned int DEBUG_LEVEL = 1;
 
 struct config {
     char *output;
-
-} config;
+    char fork_child;
+} config = {
+    .output = NULL,
+    .fork_child = 'N',
+};
 
 void print_help_and_exit(int exit_status) {
     const char* help_string =
@@ -29,8 +33,9 @@ void print_help_and_exit(int exit_status) {
         "    frzscr [OPTIONS]\n"
         "\n"
         "command line options:\n"
-        "    -h             print this help message and exit\n"
-        "    -o [OUTPUT]    only freeze this output (eg eDP-1)\n";
+        "    -h               print this help message and exit\n"
+        "    -o OUTPUT        only freeze this output (eg eDP-1)\n"
+        "    -c CMD [ARGS...] fork CMD and wait for it to exit\n";
 
     fputs(help_string, stderr);
     exit(exit_status);
@@ -39,14 +44,18 @@ void print_help_and_exit(int exit_status) {
 void parse_command_line(int *argc, char ***argv) {
     int opt;
 
-    while ((opt = getopt(*argc, *argv, ":oh")) != -1) {
+    while ((opt = getopt(*argc, *argv, ":o:ch")) != -1) {
         switch (opt) {
         case 'o':
-            debug("output name supplied on command line: %s\n", optarg);
+            info("output name supplied on command line: %s\n", optarg);
+            warn("output option not implemented!\n");
             config.output = strdup(optarg);
             break;
         case 'h':
             print_help_and_exit(0);
+            break;
+        case 'c':
+            config.fork_child = 'Y';
             break;
         case '?':
             critical("unknown option: %c\n", optopt);
@@ -64,13 +73,41 @@ void parse_command_line(int *argc, char ***argv) {
 
 int main(int argc, char **argv) {
     int exit_status = 0;
-
     int signal_fd = -1;
     int epoll_fd = -1;
+    pid_t child_pid = -1;
 
     wl_list_init(&wayland.outputs);
     wl_list_init(&windows);
     wl_list_init(&screenshots);
+
+    int child_argc = -1;
+    char **child_argv = NULL;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-c") == 0) {
+            config.fork_child = 'Y';
+            child_argc = argc - i - 1;
+            child_argv = &argv[i + 1];
+            argc = i;
+        }
+    }
+
+    parse_command_line(&argc, &argv);
+
+    info("parents args (argc = %d):\n", argc);
+    for (int i = 0; i < argc; i++) {
+        info("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    if (config.fork_child == 'Y') {
+        if (child_argc < 1) {
+            die("empty child command\n");
+        }
+        info("child args (argc = %d):\n", child_argc);
+        for (int i = 0; i < child_argc; i++) {
+            info("argv[%d]: %s\n", i, child_argv[i]);
+        }
+    }
 
     wayland_init();
 
@@ -84,12 +121,31 @@ int main(int argc, char **argv) {
         wl_list_insert(&windows, &create_window_from_screenshot(screenshot)->link);
     }
 
+    wl_display_roundtrip(wayland.display);
+
+    if (config.fork_child == 'Y') {
+        child_pid = fork();
+        switch (child_pid) {
+        case -1:
+            critical("fork() failed: %s\n", strerror(errno));
+            exit_status = 1;
+            goto cleanup;
+        case 0:
+            // child
+            execvp(child_argv[0], child_argv);
+            die("execvp() failed: %s\n", strerror(errno));
+        default:
+            // parent, just continue
+            break;
+        }
+    }
+
     /* block signals so we can catch them later */
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGINT);
     sigaddset(&mask, SIGTERM);
-    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIGCHLD);
     if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1) {
         critical("failed to block signals: %s\n", strerror(errno));
         goto cleanup;
@@ -163,13 +219,22 @@ int main(int argc, char **argv) {
                     info("received signal %d, exiting\n", signo);
                     goto cleanup;
                 case SIGCHLD:
-                    info("received SIGCHLD, not implemented\n");
-                    break;
+                    info("received SIGCHLD\n");
+                    if (child_pid < 0) {
+                        critical("but no child was created??? wtf\n");
+                        exit_status = 1;
+                        goto cleanup;
+                    }
+                    int wstatus;
+                    waitpid(child_pid, &wstatus, 0);
+                    if (WIFEXITED(wstatus)) {
+                        info("child exited with code %d\n", WEXITSTATUS(wstatus));
+                    }
+                    goto cleanup;
                 }
             }
         }
     }
-    while (wl_display_dispatch(wayland.display) != -1) {}
 
 cleanup:
     wl_list_for_each_safe(screenshot, screenshot_tmp, &screenshots, link) {
