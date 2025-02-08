78e6d0b5099bbbb0344178833bd1982fc98bba6f
heather7283 <heather7283@protonmail.com>
2025-02-08 10:26:53 +0400

    chore: consistent switch case formatting

--- utils.c
+++ utils.c
@@ -10,114 +10,114 @@ void rotate_image(void *dest, const void *src, int w, int h,
     int x, y, new_x, new_y;
 
     switch (transform) {
-        case WL_OUTPUT_TRANSFORM_NORMAL:
-            memcpy(dest, src, w * h * bytes_per_pixel);
-            break;
+    case WL_OUTPUT_TRANSFORM_NORMAL:
+        memcpy(dest, src, w * h * bytes_per_pixel);
+        break;
 
-        case WL_OUTPUT_TRANSFORM_90:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = h - 1 - y;
-                    new_y = x;
+    case WL_OUTPUT_TRANSFORM_90:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = h - 1 - y;
+                new_y = x;
 
-                    memcpy(
-                        d + (new_y * h + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * h + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
 
-        case WL_OUTPUT_TRANSFORM_180:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = w - 1 - x;
-                    new_y = h - 1 - y;
+    case WL_OUTPUT_TRANSFORM_180:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = w - 1 - x;
+                new_y = h - 1 - y;
 
-                    memcpy(
-                        d + (new_y * w + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * w + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
 
-        case WL_OUTPUT_TRANSFORM_270:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = y;
-                    new_y = w - 1 - x;
+    case WL_OUTPUT_TRANSFORM_270:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = y;
+                new_y = w - 1 - x;
 
-                    memcpy(
-                        d + (new_y * h + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * h + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
 
-        case WL_OUTPUT_TRANSFORM_FLIPPED:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = w - 1 - x;
-                    new_y = y;
+    case WL_OUTPUT_TRANSFORM_FLIPPED:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = w - 1 - x;
+                new_y = y;
 
-                    memcpy(
-                        d + (new_y * w + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * w + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
 
-        case WL_OUTPUT_TRANSFORM_FLIPPED_90:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = y;
-                    new_y = x;
+    case WL_OUTPUT_TRANSFORM_FLIPPED_90:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = y;
+                new_y = x;
 
-                    memcpy(
-                        d + (new_y * h + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * h + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
 
-        case WL_OUTPUT_TRANSFORM_FLIPPED_180:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = x;
-                    new_y = h - 1 - y;
+    case WL_OUTPUT_TRANSFORM_FLIPPED_180:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = x;
+                new_y = h - 1 - y;
 
-                    memcpy(
-                        d + (new_y * w + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * w + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
 
-        case WL_OUTPUT_TRANSFORM_FLIPPED_270:
-            for (y = 0; y < h; y++) {
-                for (x = 0; x < w; x++) {
-                    new_x = h - 1 - y;
-                    new_y = w - 1 - x;
+    case WL_OUTPUT_TRANSFORM_FLIPPED_270:
+        for (y = 0; y < h; y++) {
+            for (x = 0; x < w; x++) {
+                new_x = h - 1 - y;
+                new_y = w - 1 - x;
 
-                    memcpy(
-                        d + (new_y * h + new_x) * bytes_per_pixel,
-                        s + (y * w + x) * bytes_per_pixel,
-                        bytes_per_pixel
-                    );
-                }
+                memcpy(
+                    d + (new_y * h + new_x) * bytes_per_pixel,
+                    s + (y * w + x) * bytes_per_pixel,
+                    bytes_per_pixel
+                );
             }
-            break;
+        }
+        break;
     }
 }
 
